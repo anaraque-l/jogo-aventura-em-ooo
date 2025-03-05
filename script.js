a4e778874a865ceee54f2f386cd9b22b67dd9cd9
Ana Raquel <123raquelana321@gmail.com>
2025-03-05 20:10:15 -0300

    base animação

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-const player - document.getElementById("player");
+const player = document.getElementById("player");
 const gameContainer = document.getElementById("game-container");
 
-let playerx = 50; 
-let playery = 50; 
-let gravidade = 2; 
-let poderPular = 20; 
-let velocidadeY = 0; 
-let estaPulando = false; 
+let playerY = 300; 
+let gravidade = 2;
+let poderPular = 20;
+let velocidadeY = 0;
+let estaPulando = false;
+let speed = 5; 
+
+const elementosCenario = document.querySelectorAll(".casaArvore, .grama, .nuvem, .nuvem2, .nuvem3");
+let posicoes = {};
+
+
+elementosCenario.forEach(el => {
+    posicoes[el.id] = parseInt(getComputedStyle(el).left) || 0;
+});
+
 
 document.addEventListener("keydown", (event) => {
-    if (event.key === "" && !estaPulando){
-        estaPulando  = true; 
-        velocidadeY = - poderPular;
+    if ((event.key === " " || event.code === "Space") && !estaPulando) {
+        estaPulando = true;
+        velocidadeY = -poderPular;
     }
-
 });
 
 function updateGame() {
-    velocidadeY +=gravity;
-    playerY += velocity 
+    velocidadeY += gravidade;
+    playerY += velocidadeY;
+
+    
+    player.style.top = playerY + "px";
+
+   
+    if (playerY >= 350) {
+        playerY = 350;
+        estaPulando = false;
+        velocidadeY = 0;
+    }
+
+   
+    elementosCenario.forEach(el => {
+        posicoes[el.id] -= speed;
+        el.style.left = posicoes[el.id] + "px";
+
+       
+        if (posicoes[el.id] < -parseInt(getComputedStyle(el).width)) {
+            posicoes[el.id] = window.innerWidth;
+        }
+    });
+
+    requestAnimationFrame(updateGame);
 }
+
+updateGame();
