2bab7e06386ce70f020592d6e39d60221e045a08
Ana Raquel <123raquelana321@gmail.com>
2025-03-18 17:35:26 -0300

    Style

--- menu.js
+++ menu.js
@@ -1,12 +1,13 @@
+// Lógica de funcionamento do menu
 const startButton = document.getElementById('start-button');
 const instructionsButton = document.getElementById('instructions-button');
 
-// Botão "Iniciar Jogo"
+
 startButton.addEventListener('click', () => {
-  window.location.href = 'index.html'; // Redireciona para a página do jogo
+  window.location.href = 'index.html'; 
 });
 
-// Botão "Instruções"
+
 instructionsButton.addEventListener('click', () => {
-  alert('Instruções:\n\n1. Pressione ESPAÇO para pular.\n2. Evite os obstáculos!\n3. Salve a Princesa Jujuba!');
+  window.location.href = 'instrucoes.html'; 
 });
